ad9ed346b595768264f1abba2f0383476917b5d9
ArthurLai <cheng-yu.lai@china-airlines.com>
2015-10-22 14:00:39 +0800

    Sample Ready

--- Getting Started/Program.cs
+++ Getting Started/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Getting_Started.FlightAwareServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Getting_Started
     {
         static void Main(string[] args)
         {
+            try
+            {
+
+                FlightXML2SoapClient client = new FlightXML2SoapClient();
+
+                // Treat the test certificate as trusted
+                client.ClientCredentials.UserName.UserName = "MyUserName";
+                client.ClientCredentials.UserName.Password = "MyPassword";
+
+                EnrouteStruct r = client.Enroute("KAUS", 10, "", 0);
+                foreach (EnrouteFlightStruct e in r.enroute)
+                {
+                    System.Console.WriteLine(e.ident);
+                }
+
+                System.Console.WriteLine(client.Metar("KAUS"));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
+
+            Console.WriteLine("...");
+            Console.ReadLine();
         }
     }
 }
